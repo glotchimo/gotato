ad9a2683cce9c431e60715a997053644a3f64232
Elliott Maguire <glotchimo@pm.me>
2023-07-09 13:09:48 -0700

    Move env loading to util.go

--- main.go
+++ main.go
@@ -4,67 +4,38 @@ import (
 	"log"
 	"os"
 	"os/signal"
-	"strconv"
+
+	"github.com/gempir/go-twitch-irc/v4"
 )
 
 var (
+	// Authorization settings
 	CHANNEL       string
 	USERNAME      string
 	CLIENT_ID     string
 	CLIENT_SECRET string
 	ACCESS_TOKEN  string
 	REFRESH_TOKEN string
-	TIMER_MIN     int = 30
-	TIMER_MAX     int = 120
-	TIMEOUT       int = 30
-	REWARD        int = 100
-	COOLDOWN      int = 300
+
+	// Game settings
+	TIMER_MIN int = 30
+	TIMER_MAX int = 120
+	TIMEOUT   int = 30
+	REWARD    int = 100
+	COOLDOWN  int = 300
+
+	// Globally available Twitch IRC client
+	CLIENT *twitch.Client
 )
 
 func init() {
-	if CHANNEL = os.Getenv("GOTATO_CHANNEL"); CHANNEL == "" {
-		panic("channel cannot be blank")
-	}
-
-	if USERNAME = os.Getenv("GOTATO_USERNAME"); USERNAME == "" {
-		panic("username cannot be blank")
-	}
-
-	if CLIENT_ID = os.Getenv("GOTATO_CLIENT_ID"); CLIENT_ID == "" {
-		panic("client ID cannot be blank")
-	}
-
-	if CLIENT_SECRET = os.Getenv("GOTATO_CLIENT_SECRET"); CLIENT_SECRET == "" {
-		panic("client secret cannot be blank")
-	}
-
-	if timerMin, err := strconv.Atoi(os.Getenv("GOTATO_TIMER_MIN")); err == nil {
-		TIMER_MIN = timerMin
-	}
-
-	if timerMax, err := strconv.Atoi(os.Getenv("GOTATO_TIMER_MAX")); err == nil {
-		TIMER_MAX = timerMax
-	}
-
-	if timeout, err := strconv.Atoi(os.Getenv("GOTATO_TIMEOUT")); err == nil {
-		TIMEOUT = timeout
-	}
-
-	if reward, err := strconv.Atoi(os.Getenv("GOTATO_REWARD")); err == nil {
-		REWARD = reward
-	}
-
-	if cooldown, err := strconv.Atoi(os.Getenv("GOTATO_COOLDOWN")); err == nil {
-		COOLDOWN = cooldown
+	loadEnv()
+	if err := authorize(); err != nil {
+		log.Fatal("error authenticating:", err)
 	}
 }
 
 func main() {
-	// Authenticate with Twitch
-	if err := authorize(); err != nil {
-		log.Fatal("error authenticating:", err)
-	}
-
 	// Summarize game settings
 	log.Println("playing gotato with the following settings:")
 	log.Println("  channel:", CHANNEL)
