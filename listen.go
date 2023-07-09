63cfd12a2897fd95d4ff4dadcefb7cfdec30642f
Elliott Maguire <glotchimo@pm.me>
2023-07-08 23:56:26 -0700

    Add NOTICE listener for login failures

--- listen.go
+++ listen.go
@@ -2,28 +2,29 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/gempir/go-twitch-irc/v4"
-	"log"
 )
 
 func listen(events chan string, errors chan error) {
 	// Initialize client with a callback that listens for calls and sends IDs to game loop
 	client := twitch.NewClient(USERNAME, "oauth:"+ACCESS_TOKEN)
-	client.OnPrivateMessage(func(message twitch.PrivateMessage) {
-		log.Println("message received:", message)
 
-		if message.Message == "!gotato" {
-			events <- message.User.ID
+	// Watch for calls (PrivateMessage = any message in the channel)
+	client.OnPrivateMessage(func(m twitch.PrivateMessage) {
+		if m.Message == "!gotato" {
+			events <- m.User.ID
 		}
 	})
 
-	log.Println("Username: " + USERNAME + " Channel: " + CHANNEL)
+	// Watch for notices (i.e. login failures)
+	client.OnNoticeMessage(func(m twitch.NoticeMessage) {
+		errors <- fmt.Errorf(m.Message)
+	})
 
-	// Join channel and connect client
+	// Join channel and connect client (blocking)
 	client.Join(CHANNEL)
-	log.Println("joined channel " + CHANNEL)
 	if err := client.Connect(); err != nil {
 		errors <- fmt.Errorf("error connecting to channel: %w", err)
 	}
-	log.Println("connection established")
 }
