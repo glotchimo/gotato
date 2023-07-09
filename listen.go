fa2b0beeb28728a5256ac2acb0c02089fa9ec746
Elliott Maguire <glotchimo@pm.me>
2023-07-09 13:11:25 -0700

    Globalize IRC client for access in state loop

--- listen.go
+++ listen.go
@@ -7,25 +7,29 @@ import (
 )
 
 func listen(events chan string, errors chan error) {
-	// Initialize client with a callback that listens for calls and sends IDs to game loop
-	client := twitch.NewClient(USERNAME, "oauth:"+ACCESS_TOKEN)
+	// Initialize client with callbacks for game calls and connection issues
+	CLIENT = twitch.NewClient(USERNAME, "oauth:"+ACCESS_TOKEN)
 
-	// Watch for calls (PrivateMessage = any message in the channel)
-	client.OnPrivateMessage(func(m twitch.PrivateMessage) {
+	// Watch messages (PrivateMessage = any message in the channel)
+	CLIENT.OnPrivateMessage(func(m twitch.PrivateMessage) {
 		if m.Message == "!gotato" {
-			events <- m.User.ID
+			events <- "call:" + m.User.ID
+		} else if m.Message == "!reset" {
+			events <- "reset"
+		} else if m.FirstMessage {
+			events <- "join:" + m.User.ID
 		}
 	})
 
 	// Watch for notices (i.e. login failures)
-	client.OnNoticeMessage(func(m twitch.NoticeMessage) {
+	CLIENT.OnNoticeMessage(func(m twitch.NoticeMessage) {
 		errors <- fmt.Errorf(m.Message)
 	})
 
 	// Join channel and connect client (blocking)
-	client.Join(CHANNEL)
-	client.Say(CHANNEL, "gotato connected! Use !gotato to start a game")
-	if err := client.Connect(); err != nil {
+	CLIENT.Join(CHANNEL)
+	CLIENT.Say(CHANNEL, "gotato connected! Use !gotato to start a game")
+	if err := CLIENT.Connect(); err != nil {
 		errors <- fmt.Errorf("error connecting to channel: %w", err)
 	}
 }
