b97f80d484e75a213ec1b696d7452f526be9228d
Elliott Maguire <glotchimo@pm.me>
2023-07-09 14:46:24 -0700

    Tweak command setup in listener

--- listen.go
+++ listen.go
@@ -13,11 +13,13 @@ func listen(events chan string, errors chan error) {
 	// Watch messages (PrivateMessage = any message in the channel)
 	CLIENT.OnPrivateMessage(func(m twitch.PrivateMessage) {
 		if m.Message == "!gotato" {
-			events <- "call:" + m.User.ID
-		} else if m.Message == "!reset" {
-			events <- "reset"
-		} else if m.FirstMessage {
+			events <- "start"
+		} else if m.Message == "!pass" {
+			events <- "pass:" + m.User.ID
+		} else if m.Message == "!join" {
 			events <- "join:" + m.User.ID
+		} else if m.Message == "!reset" {
+			events <- "reset:" + m.User.Name
 		}
 	})
 
@@ -26,9 +28,14 @@ func listen(events chan string, errors chan error) {
 		errors <- fmt.Errorf(m.Message)
 	})
 
+	// Watch for timeouts
+	CLIENT.OnClearChatMessage(func(m twitch.ClearChatMessage) {
+		return
+	})
+
 	// Join channel and connect client (blocking)
 	CLIENT.Join(CHANNEL)
-	CLIENT.Say(CHANNEL, "gotato connected! Use !gotato to start a game")
+	CLIENT.Say(CHANNEL, "gotato connected")
 	if err := CLIENT.Connect(); err != nil {
 		errors <- fmt.Errorf("error connecting to channel: %w", err)
 	}
